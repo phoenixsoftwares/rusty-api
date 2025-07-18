4508194169ec11e0c9a1f366b4a99f2b66877371
Anshuman <samradni9923897009@gmail.com>
2025-07-18 22:20:59 +0530

    decimal to binary complete

--- src/b_to_d_helper/mod.rs
+++ src/b_to_d_helper/mod.rs
@@ -1,29 +1,25 @@
 #[derive(Debug, Clone, Copy)]
-pub  struct BinaryHelper {
+struct BinaryHelper {
      pub byte: i32,
      pub place: i32,
-     pub index: i32,
 }
 
 
 
-pub fn test_for_zero_one(t: i32) -> bool {
+fn test_for_zero_one(t: i32) -> bool {
     t == 1 || t == 0
 }
 
-pub fn assign_tag(t: usize, v: &mut Vec<BinaryHelper> ) {
+fn assign_tag(t: usize, v: &mut Vec<BinaryHelper> ) -> i32{
     let mut b = v[t];
     b.place = 1;
     v[t] = b;
-    // v
+    b.byte
 }
 
-pub fn new() -> Vec<BinaryHelper> {
+fn new() -> Vec<BinaryHelper> {
     let mut byte = 512;
-    let mut index = 0;
     let place = 0;
-    
-
     let mut v: Vec<BinaryHelper> = vec![];
     
     while byte != 1 {
@@ -32,10 +28,51 @@ pub fn new() -> Vec<BinaryHelper> {
         let b = BinaryHelper {
             byte,
             place,
-            index,
         };
-        index += 1;
         v.push(b);
     }
    v
+}
+
+
+pub fn decimal_to_binary(pst: i32) -> String {
+    let mut tst = pst;
+    if tst >= 256 {
+            panic!("Cannot print bigger byte = {}", tst);
+    }
+
+    if test_for_zero_one(tst) {
+        println!("Answer is {}", tst);
+        return tst.to_string();
+    }
+
+    let mut binary_vector = new();
+    let lngth = binary_vector.len();
+    let mut answer = String::new();
+
+    for _ in 0..(lngth-1) {
+        if tst == 0 {
+            break;
+        }
+        tst = tst - test_probablities(tst, lngth, &mut binary_vector);
+    }
+
+    for itr in 0..(lngth) {
+        answer.push_str(&binary_vector[itr].place.to_string());
+    }
+    answer
+}
+
+fn test_probablities(tst: i32, lngth: usize, binary_vector: &mut Vec<BinaryHelper> ) -> i32 {
+    let mut nk = 0;    
+    for itr in 0..(lngth-1) {
+        
+        if  binary_vector[itr].byte <= tst {
+            nk = assign_tag(itr, binary_vector);
+            // println!("\n\nNK {}: ", nk);
+            break
+        }
+    }
+    nk
+
 }
--- src/main.rs
+++ src/main.rs
@@ -2,31 +2,15 @@ mod b_to_d_helper;
 
 fn main() {
     let tst = 56;
+
+    // let p = (0..9).map (|n| (tst >> n) & 1);
+    // let c = 33 & 1;
+    // println!("\n\n\nBinary representation of {}: {:?}", tst, p.collect::<Vec<_>>());
+    // println!("{} & 1 = {}", c, c);
+
+
     
-    let mut binary_vector = b_to_d_helper::new();
-
-    if b_to_d_helper::test_for_zero_one(tst) {
-        println!("Answer is {}", tst);
-        return;
-    }
-
-    if tst >= 256 {
-            panic!("Cannot print bigger byte = {}", tst);
-    }
-
-    for itr in 0..(binary_vector.len()-1) {
-        
-
-        if  binary_vector[itr].byte < tst {
-            b_to_d_helper::assign_tag(itr, &mut binary_vector);
-            break
-        }
-
-    }
-    for itr in 0..(binary_vector.len()-1) {
-        println!("BinaryHelper {}: {:?}", itr, binary_vector[itr]);
-    }
-    // println!("{:?}", binary_vector);
+    println!("Answer: {}", b_to_d_helper::decimal_to_binary(tst));
 }
 
 
